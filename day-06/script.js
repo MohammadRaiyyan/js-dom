3963c236cf2d3028971561d9716c6d301370d3e6
mohammadRaiyyan <inforaiyyan@gmail.com>
2024-09-11 13:14:41 +0530

    remove all children from container

---
+++ day-06/script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector("#container");
+
+const button = document.querySelector("#removeChildren");
+
+button.addEventListener("click", function () {
+  //   const children = container.childNodes;
+  // First approach to remove all child by looping from container
+  //   children.forEach((child) => {
+  //     child.remove();
+  //   });
+  //   Second approach
+  //   container.innerHTML = null;
+});
